5fd2b6d1f6a5447876bd3b74a88098c6ded65e6a
유재석 <opp0615@naver.com>
2015-08-06 13:24:39 +0900

    solong

---
+++ 26.2(SOLONG)/26.2(SOLONG)/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+int main()
+{
+	int c = 0;//1 <= c <=10
+	int n = 0;//1<= n <= 10000
+	int m = 0;//1<= m <= 20000
+	
+
+	cin >> c;
+
+	for (int i = 0; i < c; i++)
+	{
+
+		cin >> n >> m;
+
+		for (int j = 0; j < n; j++)
+		{
+
+		}
+
+		for (int j = 0; j < m; j++)
+		{
+
+		}
+
+
+	}
+
+	return 0;
+}
